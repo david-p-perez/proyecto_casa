9be19d7cdfa6a01e1a81a0e0cd012365f4b7fff0
David_perez <grifterlol00@gmail.com>
2026-03-08 23:20:39 -0500

    inicio desarrollo de .js

--- calculator.js
+++ calculator.js
@@ -1,2 +1,21 @@
 //calculadora
-Function: suma (a,b)
+function agregarNumero(numero) {
+    document.getElementById('pantalla').value += numero;
+}
+
+function limpiar() {
+    document.getElementById('pantalla').value = '';
+}
+
+function borraruno() {
+    let pantalla = document.getElementById('pantalla');
+    pantalla.value = pantalla.value.slice(0, -1);
+}
+
+function calcular() {
+    try {
+        document.getElementById('pantalla').value = eval(document.getElementById('pantalla').value);
+    } catch {
+        document.getElementById('pantalla').value = 'Error';
+    }
+}
