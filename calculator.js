75db2ab9015ee991302262b585f933dbf4a22c68
David_perez <grifterlol00@gmail.com>
2026-03-09 00:31:07 -0500

    casi terminado el .js

--- calculator.js
+++ calculator.js
@@ -19,3 +19,25 @@ function calcular() {
         document.getElementById('pantalla').value = 'Error';
     }
 }
+
+//agregar valores a la pantalla al hacer clic en los botones
+function agregarNumero(numero) {
+    document.getElementById('pantalla').value += numero;
+}
+//limpiar la pantalla
+function limpiar() {
+    document.getElementById('pantalla').value = '';
+}
+//borrar un numero de la pantalla
+function borraruno() {
+    let pantalla = document.getElementById('pantalla');
+    pantalla.value = pantalla.value.slice(0, -1);
+}
+//calcular el resultado de la operacion
+function calcular() {
+    try {
+        document.getElementById('pantalla').value = eval(document.getElementById('pantalla').value);
+    } catch {
+        document.getElementById('pantalla').value = 'Error';
+    }
+}
